ea36b00cbfcd0242ab8203d54d3986f74157e4db
mohamedetos <etos7plus@gmail.com>
2023-03-07 21:12:45 +0200

    first commit

--- resources/views/sublimation/add_order.blade.php
+++ resources/views/sublimation/add_order.blade.php
@@ -46,25 +46,25 @@
                         {{-- 1 --}}
 
                         <div class="row mb-2">
-                            <div class="col-md-3 col-sm-6 mt-2">
+                            <div class="col-md-3 col-sm-6 mt-4">
                                 <label for="inputName" class="control-label">اسم العميل</label>
                                 <input type="text" class="form-control" id="inputName" name="invoice_number"
                                      required>
                             </div>
 
-                            <div class="col-md-3 col-sm-6 mt-2">
+                            <div class="col-md-3 col-sm-6 mt-4">
                                 <label for="inputName" class="control-label">تكرار</label>
                                 <input type="text" class="form-control" id="inputName" name="invoice_number"
                                      required>
                             </div>
 
-                            <div class="col-md-3 col-sm-6 mt-2">
+                            <div class="col-md-3 col-sm-6 mt-4">
                                 <label for="inputName" class="control-label">طول الملف</label>
                                 <input type="text" class="form-control" id="inputName" name="invoice_number"
                                      required>
                             </div>
 
-                            <div class="col-md-3 col-sm-6 mt-2">
+                            <div class="col-md-3 col-sm-6 mt-4">
                                 <label for="inputName" class="control-label">اجمالي الامتار</label>
                                 <input type="text" class="form-control" id="inputName" name="invoice_number"
                                     readonly>
@@ -73,61 +73,62 @@
                         </div>
 
                         {{-- 2 --}}
+
                         <div class="row mb-2">
-                            <div class="col-md-4 col-sm-6">
-                                <label for="inputName" class="control-label">مبلغ التحصيل</label>
-                                <input type="text" class="form-control" id="inputName" name="Amount_collection"
-                                    oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');">
+
+                        <div class="col-md-4 col-sm-6 mt-4">
+                            <label for="inputName" class="control-label">ماكينة الطباعة</label>
+                            <select name="Rate_VAT" id="Rate_VAT" class="form-control" required>
+                                <!--placeholder-->
+                                <option value="" selected disabled>حدد ماكينة الطباعه</option>
+                                <option value="Fedar">Fedar</option>
+                                <option value="DGI">DGI</option>
+                                <option value="Sky">SKY</option>
+                            </select>
+                        </div>
+
+
+                            <div class="col-md-4 col-sm-6 mt-4">
+                                <label for="inputName" class="control-label">نوع الطباعه</label>
+                                <input type="text" class="form-control" id="inputName" name="Amount_collection" placeholder="قطع / اتواب">
                             </div>
 
-                            <div class="col-md-4 col-sm-6 mt-2">
-                                <label class="control-label ">تاريخ الاوردر</label>
+                            <div class="col-md-4 col-sm-6 mt-4">
+                                <label class="control-label ">تاريخ الطباعة</label>
                                 <input class="form-control fc-datepicker" name="invoice_Date" placeholder="YYYY-MM-DD"
                                     type="text" value="{{ date('Y-m-d') }}" required>
                             </div>
 
-                            <div class="col-md-4 col-sm-6 mt-2">
-                                <label>تاريخ الاستحقاق</label>
-                                <input class="form-control fc-datepicker" name="Due_date" placeholder="YYYY-MM-DD"
-                                    type="text" required>
-                            </div>
                         </div>
-
 
                         {{-- 3 --}}
 
                         <div class="row mb-2">
 
-                            <div class="col-md-4 col-sm-6">
-                                <label for="inputName" class="control-label">مبلغ العمولة</label>
-                                <input type="text" class="form-control form-control-lg" id="Amount_Commission"
-                                    name="Amount_Commission" title="يرجي ادخال مبلغ العمولة "
-                                    oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');"
-                                    required>
-                            </div>
-
-                            <div class="col-md-4 col-sm-6">
-                                <label for="inputName" class="control-label">الخصم</label>
-                                <input type="text" class="form-control form-control-lg" id="Discount" name="Discount"
-                                    title="يرجي ادخال مبلغ الخصم "
-                                    oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');"
-                                    value=0 required>
-                            </div>
-
-                            <div class="col-md-4 col-sm-6 mt-2">
-                                <label for="inputName" class="control-label">نسبة ضريبة القيمة المضافة</label>
-                                <select name="Rate_VAT" id="Rate_VAT" class="form-control" onchange="myFunction()">
+                            <div class="col-md-4 col-sm-6 mt-4">
+                                <label for="inputName" class="control-label">المصمم</label>
+                                <select name="Rate_VAT" id="Rate_VAT" class="form-control" required>
                                     <!--placeholder-->
-                                    <option value="" selected disabled>حدد نسبة الضريبة</option>
-                                    <option value=" 5%">5%</option>
-                                    <option value="10%">10%</option>
+                                    <option value="" selected disabled>محمد محروس</option>
+                                    <option value="Fedar">مريم محمد</option>
+                                    <option value="DGI">ايه ايمن</option>
                                 </select>
+                            </div>
+
+                            <div class="col-md-4 col-sm-6 mt-4">
+                                <label for="inputName" class="control-label">القائم بالطباعه</label>
+                                <input type="text" class="form-control form-control-lg" id="Discount" value="{{Auth::User()->name}}" name="Discount" required>
+                            </div>
+
+                            <div class="col-md-4 col-sm-6 mt-4">
+                                <label for="inputName" class="control-label">رقم تلفون العميل</label>
+                                <input type="text" class="form-control form-control-lg" id="Discount"  name="Discount" required>
                             </div>
 
                         </div>
 
                         {{-- 4 --}}
-
+{{-- 
                         <div class="row mb-2">
                             <div class="col-md-6 col-sm-6">
                                 <label for="inputName" class="control-label">قيمة ضريبة القيمة المضافة</label>
@@ -138,26 +139,26 @@
                                 <label for="inputName" class="control-label">الاجمالي شامل الضريبة</label>
                                 <input type="text" class="form-control" id="Total" name="Total" readonly>
                             </div>
-                        </div>
+                        </div> --}}
 
                         {{-- 5 --}}
-                        <div class="row mt-2">
-                            <div class="col-sm-12 col-md-8 mt-2">
+                        <div class="row mt-4">
+                            <div class="col-sm-12 col-md-8 mt-4">
                                 <label for="exampleTextarea">ملاحظات</label>
                                 <textarea class="form-control" id="exampleTextarea" name="note" rows="5"></textarea>
                             </div>
 
 
 
-                            <div class="col-sm-12 col-md-4 mt-2">
-                                <label for="">المرفقات  </label>
+                            <div class="col-sm-12 col-md-4 mt-4">
+                                <label for="">صورة التصميم</label>
                                 <label for="" class="text-warning"> المتاحه Jpg / Png / Pdf / Jpeg </label>
                                 <input type="file" name="pic" class="dropify" accept=".pdf,.jpg, .png, image/jpeg, image/png"
                                     data-height="107" />
                             </div>
 
-                            <div class="col-sm-12 col-md-4 mt-2">
-                                <button type="submit" class="btn btn-outline-primary">حفظ الفاتورة</button>
+                            <div class="col-sm-12 col-md-4 mt-4">
+                                <button type="submit" class="btn btn-outline-primary ">حفظ البيانات</button>
                             </div>
                         </div>
                         <br>
